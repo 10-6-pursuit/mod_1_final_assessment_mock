9d222210e548a86f10810d3e9ff48081a54159ee
datCodeGirl <adeolamarsh@gmail.com>
2024-02-07 22:05:57 -0500

    didnt finish

--- index.js
+++ index.js
@@ -2,14 +2,40 @@
 
 // 1. Given a string of two words, both made of only lowercase letters, write a function that returns the same string, with the first letter of each word capitalized
 
-// 'mickey mouse' => 'Mickey Mouse'
+function twoWords(str){
+
+    //let newString= str[0].toUpperCase() ;
+    let newString = str[0].split(" ");
+    return newString.toUpperCase()
+
+    
+
+    return newString;
+
+}
+console.log(twoWords('mickey mouse')); // 'Mickey Mouse'
 // 'kiera gonzalez' => 'Kiera Gonzalez'
 
 // 2. Write a function that takes an array of lowercase words, return the words that have one or more 'e' but do not start with 'e'.
 
-// ["ellen", "jane", "zigland", "pedestrian"] => ["jane", "pedestrian"]
+function moreThanOne(arr){
+ let words = [];
+ let char = "e";
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i].includes(char) && arr[i][0]!==(char)){
+        
+            words.push(arr[i]);
 
-// ['red', 'lindsay', 'equine', 'drip'] => ['red'] 
+        }
+    }
+        return words;
+
+}
+
+
+//console.log(moreThanOne(["ellen", "jane", "zigland", "pedestrian"])); // ["jane", "pedestrian"]
+
+//console.log(moreThanOne(['red', 'lindsay', 'equine', 'drip'])) // ['red'] 
 
 // 3. Write a function that takes a string of lowercase letters, and returns an object with a count of how many times each letter appears
 
