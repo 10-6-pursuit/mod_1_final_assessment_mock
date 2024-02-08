787adb474a90c84ab03bfec279a21cf38eb1dedc
lynnjaco <jacobs.lynnette@gmail.com>
2024-02-07 20:57:58 -0500

    Functions Tested & Completed

--- index.js
+++ index.js
@@ -2,17 +2,29 @@
 
 // 1. Given a string of two words, both made of only lowercase letters, write a function that returns the same string, with the first letter of each word capitalized
 
+const capitalizeFirstWords = string => string.split(" ").map(ele => ele.charAt(0).toUpperCase() + ele.slice(1)).join(" ");
+console.log(capitalizeFirstWords('mickey mouse'));
+
 // 'mickey mouse' => 'Mickey Mouse'
 // 'kiera gonzalez' => 'Kiera Gonzalez'
 
+
 // 2. Write a function that takes an array of lowercase words, return the words that have one or more 'e' but do not start with 'e'.
 
-// ["ellen", "jane", "zigland", "pedestrian"] => ["jane", "pedestrian"]
+const checkForMultipleEs = array => array.filter(ele => ele.toLowerCase().split("").filter(char => char === "e").length >= 1 && ele[0] !== "e");
+console.log(checkForMultipleEs(['red', 'lindsay', 'equine', 'drip']));
 
+// ["ellen", "jane", "zigland", "pedestrian"] => ["jane", "pedestrian"]
 // ['red', 'lindsay', 'equine', 'drip'] => ['red'] 
 
 // 3. Write a function that takes a string of lowercase letters, and returns an object with a count of how many times each letter appears
 
+function generateCharCount(string) {
+    let obj = {};
+    string.split("").forEach(ele => obj[ele] = (obj[ele] || 0) + 1, {});
+    return obj;
+}
+console.log(generateCharCount("asciiaske"));
 // 'asciiaske' => {
 //   'a' : 2, 
 //   's' : 2,
