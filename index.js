d9b6e1a4db5baf1afcb4bc44d2bf2b0038e155e5
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-02-07 21:38:52 -0500

    first and second function done

--- index.js
+++ index.js
@@ -5,11 +5,35 @@
 // 'mickey mouse' => 'Mickey Mouse'
 // 'kiera gonzalez' => 'Kiera Gonzalez'
 
+const capEveryWord = str => {
+    let words = str.split(" "), capWord = [];
+    for(let x of words){
+       capWord.push(x[0].toUpperCase() + x.slice(1))
+    }
+    return capWord.join(" ");
+}
+
+
 // 2. Write a function that takes an array of lowercase words, return the words that have one or more 'e' but do not start with 'e'.
 
 // ["ellen", "jane", "zigland", "pedestrian"] => ["jane", "pedestrian"]
 
 // ['red', 'lindsay', 'equine', 'drip'] => ['red'] 
+
+const wordsWithEThatDontStartWithE = arr =>{
+   let notEAtBegining = arr.filter(word => word[0] !== "e");
+   let withE = [];
+   for (let x of notEAtBegining){
+    for(let letter of x){
+        if(letter === "e"){
+            withE.push(x)
+            break;
+        }
+    }
+   }
+   return withE;
+}
+
 
 // 3. Write a function that takes a string of lowercase letters, and returns an object with a count of how many times each letter appears
 
@@ -21,3 +45,4 @@
 //   'k' : 1, 
 //   'e' : 1
 // }
+
