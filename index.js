7a9651c2211a4ccb4e76789c89c9810967ff991c
franklinTaylor <ftaylor07@icloud.com>
2024-02-07 21:42:14 -0500

    solutions for 1 and 3

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // Sample Module 1 Assessment:
 
 // 1. Given a string of two words, both made of only lowercase letters, write a function that returns the same string, with the first letter of each word capitalized
+function captilizeWords(str) {
+    return str.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
 
+}
+
+console.log(captilizeWords('Mickey Mouse'));
 // 'mickey mouse' => 'Mickey Mouse'
 // 'kiera gonzalez' => 'Kiera Gonzalez'
 
 // 2. Write a function that takes an array of lowercase words, return the words that have one or more 'e' but do not start with 'e'.
+
+// loop thru arr of Strings
+// compare to see if the words have more than one e but do not starr with an e
+// push them to my new array
+
+
+function wordsWithMultiple(arrOflowerCase) {
+    
+} 
+
+// 
 
 // ["ellen", "jane", "zigland", "pedestrian"] => ["jane", "pedestrian"]
 
@@ -21,3 +37,18 @@
 //   'k' : 1, 
 //   'e' : 1
 // }
+
+function letterCount(inputStr) {
+    let newObj = {}
+
+    for (let str of inputStr) {
+        if (newObj[str]) {
+            newObj[str]++
+        } else {
+            newObj[str] = 1
+        }
+    }
+    return newObj
+}
+
+console.log(letterCount('asciiaske'));
